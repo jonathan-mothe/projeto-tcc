b97bd8d403f387313208ddc9ac0ee078fecbbf53
Jonathan Mothé <jonathan.mothe@gmail.com>
2020-01-20 21:21:52 -0300

    alteraçao do tema do admin

--- papyrus/settings.py
+++ papyrus/settings.py
@@ -129,7 +129,7 @@ MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 
-JET_DEFAULT_THEME = 'white'
+JET_DEFAULT_THEME = 'light-gray'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
