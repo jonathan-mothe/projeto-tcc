5b5e619b01c0c76b9c3522bdf74a3cd42b3589e7
Jonathan Mothé <jonathan.mothe@gmail.com>
2020-02-05 23:43:02 -0300

    criaçao dos components App e Header

---
+++ frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './Header';
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <Header />
+        );
+    }
+}
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Header extends React.Component{
+    render(){
+        return(
+            <div className="text-center">
+                <h1>Papyrus</h1>
+                <h3>Repositório de trabalhos acadêmicos</h3>
+            </div>
+        );
+    }
+}
