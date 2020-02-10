2241ddb09ecba0c7dbd056033fb1b36dd69ca36f
jonathan-mothe <jonathan.mothe@gmail.com>
2020-02-10 18:00:56 -0300

    alterando linha de codigo jsx

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Header extends React.Component{
     render(){
         return(
-            <div className="text-center">
+            <div>
                 <h1>Papyrus</h1>
                 <h3>Repositório de trabalhos acadêmicos</h3>
             </div>
