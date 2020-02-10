9e0cea24d4c37fcdee3d58c3168eda859daac816
jonathan-mothe <jonathan.mothe@gmail.com>
2020-02-10 19:18:20 -0300

    Testando css nos componentes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import './App.css';
 
 export default class App extends React.Component{
     render(){
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,9 +3,10 @@ import React from 'react';
 export default class Header extends React.Component{
     render(){
         return(
-            <div>
-                <h1>Papyrus</h1>
-                <h3>Repositório de trabalhos acadêmicos</h3>
+            <div className="navmenu">
+                Home
+                Sobre
+                Contato
             </div>
         );
     }
