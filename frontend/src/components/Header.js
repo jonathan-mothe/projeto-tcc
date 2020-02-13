d2c15c42905f0be00f6912026b047060fbdc14b7
jonathan-mothe <jonathan.mothe@gmail.com>
2020-02-13 19:36:19 -0300

    alterações no css e tema do admin

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,9 +4,13 @@ export default class Header extends React.Component{
     render(){
         return(
             <div className="navmenu">
-                Home
-                Sobre
-                Contato
+                <div id="menu">
+                <a href="http://localhost:3000/">Home</a>
+                <a href="http://www.globoesporte.com/botafogo">Sobre</a></div>
+                <div id="login">
+                    <a href="http://127.0.0.1:8000/admin">Login</a>
+                </div>
+
             </div>
         );
     }
