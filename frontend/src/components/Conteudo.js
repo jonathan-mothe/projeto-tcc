10403aa73a445a204c0ad2f5d5fec5d4e196c4d3
jonathan-mothe <jonathan.mothe@gmail.com>
2020-03-03 21:16:29 -0300

    Editando component Conteudo

--- frontend/src/components/Conteudo.js
+++ frontend/src/components/Conteudo.js
@@ -5,10 +5,50 @@ export default class Conteudo extends React.Component{
         return(
             <main className="content">
 				<div class="listas">
-                    <div class="lista"></div>
-                    <div class="lista"></div>
-                    <div class="lista"></div>
-                    <div class="lista"></div>
+                    <div class="lista">
+                        <h3>Trabalhos de Conclusão de Curso</h3>
+                    </div>
+
+                    <div class="lista">
+                    <table>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </table>
+                        
+                    </div>
+
+                    <div class="lista">
+                    <table>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </table>
+                    </div>
+
+                    <div class="lista">
+                    <table>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </table>
+                    </div>
+                    
                 </div>
 	
             </main>
